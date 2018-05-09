2af80b086f89809bc59a1faaa714687a17ab8068
FReddy <freddy.lemus@beitech.co>
2018-05-09 14:50:07 -0500

    Add repository

--- AlMundoRestAPI/services/hotelServices.js
+++ AlMundoRestAPI/services/hotelServices.js
@@ -1,5 +1,5 @@
 const fs = require('fs');
-
+const db = require('.././repository/mongodb-connect');
 
 var fetchHotels = () => {
   try {
@@ -12,17 +12,36 @@ var fetchHotels = () => {
 };
 
 var getHotelByNameAndStars = (name, stars) => {
-  return fetchHotels().filter(  hotel => 
-                                  (     ( isEmpty(name)  || hotel.name.toUpperCase().startsWith(name.toUpperCase()) ) 
-                                    &&  ( isEmpty(stars) || (stars).includes(hotel.stars) ) 
-                                  ) 
+  return fetchHotels().filter(  hotel =>
+                                  (     ( isEmpty(name)  || hotel.name.toUpperCase().startsWith(name.toUpperCase()) )
+                                    &&  ( isEmpty(stars) || (stars).includes(hotel.stars) )
+                                  )
                         );
 };
 
 function isEmpty(param) {
   return param == undefined || param == '' || param  == 'null'|| param  == 'undefined' ;
+};
+
+
+var getHotelByNameAndStars2 = (name, stars) => {
+  return db.getConnection().collection('collectionHotels')
+            .find( { "stars": { $in: num } ,
+                     "name" : {'$regex' : '1', '$options' : 'i'}  } ).toArray()
+            .then((docs) =>
+              {
+                 console.log(" list " + JSON.stringify(docs, undefined, 2) );
+               }, (err) => {
+                 console.log('Error ' + err);
+               }
+
+              );
+
 }
 
 module.exports = {
   getHotelByNameAndStars
-}
+};
+
+
+getHotelByNameAndStars2('l','1,2');
